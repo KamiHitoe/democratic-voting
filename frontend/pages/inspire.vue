c040ba51acc866eea07afb06e27706ec86e5ad0f
Alice <sousaku_0912@yahoo.co.jp>
2022-01-01 19:45:02 +0900

    first commit

---
+++ frontend/pages/inspire.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <h3>this is test page</h3>
+    <NuxtLink class="jump" to="/">jump home</NuxtLink>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'InspirePage',
+  created() {
+    this.showString("say hi");
+  },
+  methods: {
+    showString(str: string) {
+      console.log(str.toUpperCase());
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.jump {
+  background: pink;
+  &:hover {
+    background: green;
+  }
+}
+
+</style>
