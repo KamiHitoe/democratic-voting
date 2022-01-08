f47462075c654292cf8d453aa25093fd0f4d5cd5
KamiHitoe <sousaku_0912@yahoo.co.jp>
2022-01-09 03:32:33 +0900

    add docker-compose and docker-entrypoint.sh

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-rm -f tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
 rails db:create && rails db:migrate && rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
