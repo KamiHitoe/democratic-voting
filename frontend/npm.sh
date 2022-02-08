744d6f07510bb71a6bd0c19e897a9b0a460ef328
Alice <sousaku_0912@yahoo.co.jp>
2022-02-08 12:25:40 +0900

    m package.json

--- frontend/npm.sh
+++ frontend/npm.sh
@@ -1,5 +1,11 @@
 
-npm install -D 
+# yarn dev
+npm install -D @nuxt/typescript-build
+# yarn lint
+npm install -D @nuxtjs/eslint-config-typescript @nuxtjs/eslint-module babel-eslint eslint eslint-config-prettier eslint-loader eslint-plugin-nuxt eslint-plugin-prettier prettier
+# yarn test
+npm install -D @babel/core @babel/preset-env babel-core@^7.0.0-bridge.0 @vue/test-utils jest babel-jest jest-serializer-vue vue-jest ts-jest
+
 npm uninstall -D
 
 @babel/core
@@ -15,8 +21,8 @@ eslint-config-prettier
 eslint-loader
 eslint-plugin-nuxt
 eslint-plugin-prettier
-jest
 prettier
+jest
 ts-jest
 vue-jest
 
