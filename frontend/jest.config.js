a1953bdccab9278a5a5436cb0308ffe902e73301
Starmiya☆Miyuki <49331725+KamiHitoe@users.noreply.github.com>
2022-02-12 19:32:14 +0900

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  // globalSetup: "<rootDir>/test/setup.ts",
-  // globalTeardown: "<rootDir>/test/teardown.ts",
-  // mapCoverage: true,
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$",
-  // moduleNameMapper: {
-  //   "^@/(.*)$": "<rootDir>/$1",
-  //   "^~/(.*)$": "<rootDir>/$1",
-  //   "^vue$": "vue/dist/vue.common.js",
-  // },
-  moduleFileExtensions: ["ts", "js", "vue", "json"],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'vue',
+    'json'
+  ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    ".*\\.(vue)$": "babel-jest",
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    "<rootDir>/components/**/*.vue",
-    "<rootDir>/pages/**/*.vue",
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
   ],
-  transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash-es/.*)"],
-  testEnvironment: "jsdom",
-};
+  testEnvironment: 'jsdom'
+}
