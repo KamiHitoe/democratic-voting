4e93077459d416d9401b814cbba9b26cf4214b28
KamiHitoe <sousaku_0912@yahoo.co.jp>
2022-02-14 21:41:52 +0900

    doing rewrite test in RSpec

--- backend/spec/requests/v1/comment_spec.rb
+++ backend/spec/requests/v1/comment_spec.rb
@@ -5,20 +5,22 @@ RSpec.describe(Comment, type: :request) do
     before(:each) do
       @user = create(:user)
       @topic = create(:topic)
-      @comment = create(:comment, topic_id: @topic.id)
+      @comments = create_list(:comment, 33, topic_id: @topic[:id])
     end
 
     it 'get all comments' do
-      get "/v1/topics/#{@topic.id}/comments"
+      # TopicにたいしてCommentsのArrayが取得できることを確認
+      get "/v1/topics/#{@topic[:id]}/comments"
       json = JSON.parse(response.body)
       expect(response.status).to(eq(200))
-      # puts response.body
+      expect(json).not_to be_empty
     end
 
     it 'get a comment by id' do
-      get "/v1/topics/#{@topic.id}/comments/#{@comment.id}"
+      get "/v1/topics/#{@topic[:id]}/comments/#{@comments[0][:id]}"
       json = JSON.parse(response.body)
       expect(response.status).to(eq(200))
+      expect(json).to be_kind_of(Hash)
     end
 
     it 'create a new comment' do
@@ -26,36 +28,36 @@ RSpec.describe(Comment, type: :request) do
       old_user = create(:user, age: 40)
       # valid user
       expect do
-        post("/v1/topics/#{@topic.id}/comments", params: {
-               user_id: @user.id,
+        post("/v1/topics/#{@topic[:id]}/comments", params: {
+               user_id: @user[:id],
                text: 'new text'
              })
       end.to(change(Comment, :count).by(1))
       # invalid sex
       expect do
-        post("/v1/topics/#{@topic.id}/comments", params: {
-               user_id: male_user.id,
+        post("/v1/topics/#{@topic[:id]}/comments", params: {
+               user_id: male_user[:id],
                text: 'new text'
              })
       end.to(change(Comment, :count).by(0))
       # invalid age
       expect do
-        post("/v1/topics/#{@topic.id}/comments", params: {
-               user_id: old_user.id,
+        post("/v1/topics/#{@topic[:id]}/comments", params: {
+               user_id: old_user[:id],
                text: 'new text'
              })
       end.to(change(Comment, :count).by(0))
     end
 
     # it 'update comment' do
-    #   put "/v1/topics/#{@topic.id}/comments/#{@comment.id}", params: { like_num: 1 }
+    #   put "/v1/topics/#{@topic[:id]}/comments/#{@comment[:id]}", params: { like_num: 1 }
     #   # response.body is not exist
     #   expect(response).to(be_successful)
     # end
 
     it 'get replied comments by parent comment id' do
-      reply_comment = Comment.create(parent_id: @comment.id, text: "test reply comment")
-      get "/v1/comments/#{@topic.id}/#{@comment.id}"
+      reply_comment = Comment.create(parent_id: @comments[0][:id], text: "test reply comment")
+      get "/v1/comments/#{@topic[:id]}/#{@comments[0][:id]}"
       expect(response.status).to(eq(200))
     end
 
