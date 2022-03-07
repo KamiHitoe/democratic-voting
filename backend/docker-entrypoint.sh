4456d55b8ed60f05272dff80dcf8318c96172f54
KamiHitoe <sousaku_0912@yahoo.co.jp>
2022-03-07 13:12:16 +0900

    m

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 # 2回目以降のpushでは不要
-# rails db:create db:migrate db:seed RAILS_ENV=production
-rails db:create && rails db:migrate:reset && rails db:seed
+rails db:create db:migrate db:seed RAILS_ENV=production
+# rails db:create && rails db:migrate:reset && rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
